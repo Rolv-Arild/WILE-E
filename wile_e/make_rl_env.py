cb59f25ecbea375e3c319ed73e9fb9663a4b936f
Rolv-Arild <rolv_arild@hotmail.com>
2024-03-02 23:40:47 +0100

    Add copy of lookup parser with assertion message

--- wile_e/make_rl_env.py
+++ wile_e/make_rl_env.py
@@ -3,7 +3,6 @@ from typing import Dict, Any
 import numpy as np
 from dm_env import specs
 from rlgym.api import StateMutator, StateType, ActionParser, AgentID, SpaceType
-from rlgym.rocket_league.action_parsers import LookupTableAction
 from rlgym.rocket_league.api import GameState
 from rlgym.rocket_league.done_conditions import GoalCondition, NoTouchTimeoutCondition, TimeoutCondition
 from rlgym.rocket_league.obs_builders import DefaultObs
@@ -19,6 +18,63 @@ class FixKeyMutator(StateMutator[GameState]):
     def apply(self, state: StateType, shared_info: Dict[str, Any]) -> None:
         for key in list(state.cars.keys()):
             state.cars[key.replace("-", "_")] = state.cars.pop(key)
+
+
+class FixedLookupTableAction(ActionParser[AgentID, np.ndarray, np.ndarray, GameState, int]):
+    """
+    World-famous discrete action parser which uses a lookup table to reduce the number of possible actions from 1944 to 90
+    """
+
+    def __init__(self):
+        super().__init__()
+        self._lookup_table = self.make_lookup_table()
+
+    def get_action_space(self, agent: AgentID) -> int:
+        return len(self._lookup_table)
+
+    def reset(self, initial_state: GameState, shared_info: Dict[str, Any]) -> None:
+        pass
+
+    def parse_actions(self, actions: Dict[AgentID, np.ndarray], state: GameState, shared_info: Dict[str, Any]) -> Dict[AgentID, np.ndarray]:
+        parsed_actions = {}
+        for agent, action in actions.items():
+            # Action can have shape (Ticks, 1) or (Ticks)
+            assert len(action.shape) == 1 or (len(action.shape) == 2 and action.shape[1] == 1), (
+                "Action can have shape (Ticks, 1) or (Ticks), got: {}".format(action.shape))
+
+            if len(action.shape) == 2:
+                action = action.squeeze(1)
+
+            parsed_actions[agent] = self._lookup_table[action]
+
+        return parsed_actions
+
+    @staticmethod
+    def make_lookup_table():
+        actions = []
+        # Ground
+        for throttle in (-1, 0, 1):
+            for steer in (-1, 0, 1):
+                for boost in (0, 1):
+                    for handbrake in (0, 1):
+                        if boost == 1 and throttle != 1:
+                            continue
+                        actions.append([throttle or boost, steer, 0, steer, 0, 0, boost, handbrake])
+        # Aerial
+        for pitch in (-1, 0, 1):
+            for yaw in (-1, 0, 1):
+                for roll in (-1, 0, 1):
+                    for jump in (0, 1):
+                        for boost in (0, 1):
+                            if jump == 1 and yaw != 0:  # Only need roll for sideflip
+                                continue
+                            if pitch == roll == jump == 0:  # Duplicate with ground
+                                continue
+                            # Enable handbrake for potential wavedashes
+                            handbrake = jump == 1 and (pitch != 0 or yaw != 0 or roll != 0)
+                            actions.append([boost, yaw, pitch, yaw, roll, jump, boost, handbrake])
+
+        return np.array(actions)
 
 
 class FixedRepeatAction(ActionParser[AgentID, np.ndarray, np.ndarray, GameState, SpaceType]):
@@ -58,7 +114,7 @@ def make_rl_environment(seed):
     state_mutators = [FixedTeamSizeMutator(), KickoffMutator(), FixKeyMutator()]
     obs_builder = DefaultObs(zero_padding=1)
     tick_skip = 8
-    action_parser = FixedRepeatAction(LookupTableAction(), tick_skip)
+    action_parser = FixedRepeatAction(FixedLookupTableAction(), tick_skip)
     reward_function = CombinedReward(
         (VelocityPlayerToBallReward(), tick_skip / 120),
         (TouchReward(), tick_skip / 120),
